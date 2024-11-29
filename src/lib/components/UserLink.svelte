8617bc2861afbb49642d91adffa9795b1f99dfec
danilasar <danila.sar@yandex.ru>
2024-11-29 18:31:13 +0400

    фича: карточки

---
+++ src/lib/components/UserLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button/index.js';
+  import * as Avatar from '$lib/components/ui/avatar/index.js';
+</script>
+
+<Button
+  variant="link"
+  style="padding-left: 0">
+  <Avatar.Root class="mr-2">
+    <Avatar.Image
+      src="https://github.com/shadcn.png"
+      alt="@shadcn" />
+    <Avatar.Fallback>CN</Avatar.Fallback>
+  </Avatar.Root>
+  @ML_PRRIDE
+</Button>
---
+++ src/lib/modules/RadioPoll.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button/index.js';
+
+  import * as RadioGroup from '$lib/components/ui/radio-group/index.js';
+  import { Label } from '$lib/components/ui/label/index.js';
+</script>
+
+<RadioGroup.Root
+  style="margin-bottom: 1rem"
+  value="comfortable">
+  <div class="flex items-center space-x-2">
+    <RadioGroup.Item
+      value="default"
+      id="r1" />
+    <Label for="r1">Гей</Label>
+  </div>
+  <div class="flex items-center space-x-2">
+    <RadioGroup.Item
+      value="comfortable"
+      id="r2" />
+    <Label for="r2">Пидорас</Label>
+  </div>
+  <div class="flex items-center space-x-2">
+    <RadioGroup.Item
+      value="compact"
+      id="r3" />
+    <Label for="r3">Гомосек</Label>
+  </div>
+  <RadioGroup.Input name="spacing" />
+</RadioGroup.Root>
+<Button>Дать голос</Button>
