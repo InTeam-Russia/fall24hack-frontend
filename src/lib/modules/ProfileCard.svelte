1d925cfd7476a7121e47a8d0a4e9ac43f11e2cbb
tolstovr <robertproducts2021@gmail.com>
2024-11-29 17:21:11 +0400

    feat: add basic feed && profile card

---
+++ src/lib/modules/ProfileCard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+  import { Card } from '$lib/components/ui/card';
+  import type { User } from '$lib/stores/userStore';
+  import { AtSign, Mail, MessageSquareMore } from 'lucide-svelte';
+
+  export let CardProps: User;
+</script>
+
+<Card class="mx-2 my-2 rounded-lg py-3 px-4 shadow-md flex-1 flex flex-col justify-between">
+  <div>
+    <h1 class="font-bold text-xl">{CardProps.firstName} {CardProps.lastName}</h1>
+    <small class="flex flex-row items-center text-zinc-500 mt-2">
+      <AtSign class="h-3 w-3 mr-[.25rem]" />
+      {CardProps.username}
+    </small>
+    <small class="flex flex-row items-center text-zinc-500">
+      <Mail class="h-3 w-3 mr-[.25rem]" />
+      {CardProps.email}
+    </small>
+    <small class="flex flex-row items-center text-zinc-500">
+      <MessageSquareMore class="h-3 w-3 mr-[.25rem]" />
+      {CardProps.tgLink}
+    </small>
+  </div>
+
+  <Button
+    size="sm"
+    class="mt-4 text-xs"
+    href={`https://${CardProps.tgLink}`}
+    target="_blank">Контакт!</Button>
+</Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,48 @@
-<script>
-  import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
-  import { userStore } from '$lib/stores/userStore';
+<script lang="ts">
+  import ProfileCard from '$lib/modules/ProfileCard.svelte';
+  import { userStore, type User } from '$lib/stores/userStore';
+
+  type UserOrPoll = User[];
+
+  let feed: UserOrPoll[] = [
+    [
+      {
+        firstName: 'Иван',
+        lastName: 'Афоничев',
+        username: 'iafonichev',
+        email: 'iafonichev@gmail.com',
+        tgLink: 't.me/BoringPlace',
+      },
+      {
+        firstName: 'Марк',
+        lastName: 'Даун',
+        username: 'markdaun',
+        email: 'markdaun@gmail.com',
+        tgLink: 't.me/markdaun',
+      },
+      {
+        firstName: 'Яна',
+        lastName: 'Цист',
+        username: 'cistit',
+        email: 'agile@nigga.com',
+        tgLink: 't.me/fuckmeplsomg',
+      },
+    ],
+  ];
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<ThemeSwitcher />
+<main class="items-stretch max-w-screen-sm mt-6 md:mt-16 mx-auto">
+  {#each feed as item}
+    {#if item[0] && 'firstName' in item[0]}
+      <div class="flex flex-col sm:flex-row gap-2 justify-between w-full">
+        <ProfileCard CardProps={item[0] as unknown as User} />
+        <ProfileCard CardProps={item[1] as unknown as User} />
+        <ProfileCard CardProps={item[2] as unknown as User} />
+      </div>
+    {/if}
+  {/each}
+</main>
 
-{JSON.stringify($userStore)}
+<div class="w-full overflow-hidden">
+  {JSON.stringify($userStore)}
+</div>
