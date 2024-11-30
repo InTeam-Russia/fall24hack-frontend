fe7964164f81bc6ab652cadd49a866f22dcd422a
tolstovr <robertproducts2021@gmail.com>
2024-11-30 11:15:25 +0400

    refactor(polls): add mocks and templates

--- src/lib/modules/TextPoll.svelte
+++ src/lib/modules/TextPoll.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
   import { Button } from '$lib/components/ui/button/index.js';
   import { Label } from '$lib/components/ui/label/index.js';
-
   import { Textarea } from '$lib/components/ui/textarea/index.js';
+
+  const placeholders = ['Я считаю, что ...', 'По моим убеждениям, ...', 'Статистически, ...'];
+
+  export let value: string;
+  export let handleSubmit: () => void;
 </script>
 
 <div
   class="grid w-full gap-1.5"
   style="margin-bottom: 1rem">
-  <Label>Твой ответ:</Label>
+  <Label class="ml-[.25rem]">Твой ответ:</Label>
   <Textarea
-    placeholder="У попа была собака..."
-    class="max-h-[10rem]" />
+    placeholder={placeholders[Math.floor(Math.random() * placeholders.length)]}
+    class="max-h-[10rem]"
+    bind:value />
 </div>
-<Button>Ответить</Button>
+
+<Button
+  size="sm"
+  class="text-xs"
+  target="_blank"
+  on:click={handleSubmit}>Ответить!</Button>
