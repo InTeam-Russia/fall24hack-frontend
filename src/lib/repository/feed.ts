30a4fbfe4f920f3a3fa8dca3c205768a8aa75c82
danilasar <danila.sar@yandex.ru>
2024-11-30 11:38:19 +0400

    фикс: баг с интерфейсом Feed

--- src/lib/repository/feed.ts
+++ src/lib/repository/feed.ts
@@ -11,54 +11,54 @@ export default class FeedRepository {
   }
   public async getFeed(): Promise<Feed[]> {
     return [
-		{
-		  id: 1,
-		  type: 'radio',
-		  text: 'Выбери свою ориентацию (иначе ты пидорас)',
-		  variants: ['Гей', 'Пидорас', 'Гомосек'],
-		  author: {
-			firstName: 'Иван',
-			lastName: 'Афоничев',
-			username: 'iafonichev',
-			email: 'iafonichev@gmail.com',
-			tgLink: 't.me/BoringPlace',
-		  },
-		},
-		[
-		  {
-			firstName: 'Иван',
-			lastName: 'Афоничев',
-			username: 'iafonichev',
-			email: 'iafonichev@gmail.com',
-			tgLink: 't.me/BoringPlace',
-		  },
-		  {
-			firstName: 'Марк',
-			lastName: 'Даун',
-			username: 'markdaun',
-			email: 'markdaun@gmail.com',
-			tgLink: 't.me/markdaun',
-		  },
-		  {
-			firstName: 'Яна',
-			lastName: 'Цист',
-			username: 'cistit',
-			email: 'agile@nigga.com',
-			tgLink: 't.me/fuckmeplsomg',
-		  },
-		],
-		{
-		  id: 2,
-		  type: 'text',
-		  text: 'Опиши свои чувства по отношению к Третьему рейху',
-		  author: {
-			firstName: 'Иван',
-			lastName: 'Афоничев',
-			username: 'iafonichev',
-			email: 'iafonichev@gmail.com',
-			tgLink: 't.me/BoringPlace',
-		  },
-		},
-	  ];
+      {
+        id: 1,
+        type: 'radio',
+        text: 'Выбери свою ориентацию (иначе ты пидорас)',
+        variants: ['Гей', 'Пидорас', 'Гомосек'],
+        author: {
+          firstName: 'Иван',
+          lastName: 'Афоничев',
+          username: 'iafonichev',
+          email: 'iafonichev@gmail.com',
+          tgLink: 't.me/BoringPlace',
+        },
+      },
+      [
+        {
+          firstName: 'Иван',
+          lastName: 'Афоничев',
+          username: 'iafonichev',
+          email: 'iafonichev@gmail.com',
+          tgLink: 't.me/BoringPlace',
+        },
+        {
+          firstName: 'Марк',
+          lastName: 'Даун',
+          username: 'markdaun',
+          email: 'markdaun@gmail.com',
+          tgLink: 't.me/markdaun',
+        },
+        {
+          firstName: 'Яна',
+          lastName: 'Цист',
+          username: 'cistit',
+          email: 'agile@nigga.com',
+          tgLink: 't.me/fuckmeplsomg',
+        },
+      ],
+      {
+        id: 2,
+        type: 'text',
+        text: 'Опиши свои чувства по отношению к Третьему рейху',
+        author: {
+          firstName: 'Иван',
+          lastName: 'Афоничев',
+          username: 'iafonichev',
+          email: 'iafonichev@gmail.com',
+          tgLink: 't.me/BoringPlace',
+        },
+      },
+    ];
   }
 }
