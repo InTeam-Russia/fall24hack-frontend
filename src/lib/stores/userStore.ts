a1117ea47297b4da5aa31ac720bfcaafcd3fa766
danilasar <danila.sar@yandex.ru>
2024-11-30 16:01:00 +0400

    сука conventional commits в пизду все тут линтер заебал блять

--- src/lib/stores/userStore.ts
+++ src/lib/stores/userStore.ts
@@ -2,6 +2,9 @@ import { Users } from '$lib/repository/users';
 import type { User } from '$lib/utils/types';
 import { writable } from 'svelte/store';
 
-export const userStore = writable<User | null>(await Users.Instance.GetSession());
+export const userStore = writable<User | null>(null);
 
+(async () => {
+  userStore.set(await Users.Instance.GetSession());
+})();
 // export const userStore = writable<User | null>(null);
